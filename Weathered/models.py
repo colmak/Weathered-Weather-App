5b81d8e432f7f4d07448381ed4ecd41c13797328
colmak <69098858+colmak@users.noreply.github.com>
2022-12-07 18:45:35 -0500

    fixing database, and starting creating of profile system

--- Weathered/models.py
+++ Weathered/models.py
@@ -1,19 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
-
-REACTION = (
-    ('Smile', 'Smile'),
-    ('Neutral', 'Neutral'),
-    ('Sad', 'Sad'),
-)
 
 
 class Reaction(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
-    location = models.CharField(max_length=200)
-    reaction = models.CharField(max_length=20, blank=True, choices=REACTION)
+    location = models.TextField(max_length=200)
+    reaction = models.TextField(max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.reaction
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    location = models.CharField(max_length=200)
+    bio = models.TextField(max_length=200)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Weather(models.Model):
--- WeatheredRepo/urls.py
+++ WeatheredRepo/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('login/', views.login_view, name="login"),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.register_view, name="register"),
+    path('profile/', views.profile_view, name='profile'),
     path('topweather/', views.topweather, name="topweather")
 ]
