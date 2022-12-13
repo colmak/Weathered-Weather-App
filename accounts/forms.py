56e4fefae1ba57c1f0dc1a6b113023743ff289ef
colmak <69098858+colmak@users.noreply.github.com>
2022-12-12 22:04:02 -0500

    Profile page UI improved

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,7 +14,7 @@ class UpdateUserForm(forms.ModelForm):
 
 class UpdateProfileForm(forms.ModelForm):
     profile_pic = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
-    about = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+    about = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 3}))
 
     class Meta:
         model = Profile
